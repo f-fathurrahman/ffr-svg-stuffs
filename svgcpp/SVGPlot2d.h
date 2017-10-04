8cb5f5d27291b3e99df9c9b687ff8ceed26dd749
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2017-10-04 15:52:30 +0700

    Add SVGPlot2d

---
+++ svgcpp/SVGPlot2d.h
@@ -0,0 +1,11 @@
+#include <string>
+
+using namespace std;
+
+void draw_line( double x1, double y1, double x2, double y2, string style );
+
+void draw_circle( double cx, double cy, double r, string style );
+
+void draw_polyline( double *pts, string style );
+
+
