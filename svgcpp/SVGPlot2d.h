d81168930054bd475baba4d8ee9feaf66d67099b
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2017-10-05 09:24:24 +0700

    Test SVGPlot2d

--- svgcpp/SVGPlot2d.h
+++ svgcpp/SVGPlot2d.h
@@ -1,11 +1,76 @@
+#ifndef __SVGPLOT2D__
+#define __SVGPLOT2D__
+
 #include <string>
+#include <iostream>
 
 using namespace std;
 
-void draw_line( double x1, double y1, double x2, double y2, string style );
+class SVGDraw2d
+{
 
-void draw_circle( double cx, double cy, double r, string style );
+public:
 
-void draw_polyline( double *pts, string style );
+  SVGDraw2d();
+  ~SVGDraw2d();
 
+  string str;
+
+  void write();
+  void draw_line( double x1, double y1, double x2, double y2, string style );
+  void draw_circle( double cx, double cy, double r, string style );
+  void draw_polyline( double *pts, string style );
+
+};
+
+// Constructor
+SVGDraw2d::SVGDraw2d()
+{
+  cout << "Constructor is called" << endl;
+  str = "<svg width=\"140\" height=\"170\"xmlns=\"http://www.w3.org/2000/svg\"\n";
+  str += "xmlns:xlink=\"http://www.w3.org/1999/xlink\">\n";
+}
+
+// Destructor
+SVGDraw2d::~SVGDraw2d()
+{
+  cout << "Destructor is called" << endl;
+}
+
+void SVGDraw2d::write()
+{
+  str += "</svg>\n";
+  cout << str;
+}
+
+
+void SVGDraw2d::draw_line( double x1, double y1, double x2, double y2, string style )
+{
+
+}
+
+void SVGDraw2d::draw_circle( double cx, double cy, double r, string style )
+{
+
+}
+
+void SVGDraw2d::draw_polyline( double *pts, string style )
+{
+
+}
+
+
+//
+// A class for to make 2d plot
+//
+
+class SVGPlot2d
+{
+public:
+  SVGPlot2d();
+  ~SVGPlot2d();
+};
+
+
+#endif
 
---
+++ svgcpp/test.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+#include "SVGPlot2d.h"
+
+using namespace std;
+
+int main()
+{
+  SVGDraw2d plt;
+  plt.write();
+
+  return 0;
+}
